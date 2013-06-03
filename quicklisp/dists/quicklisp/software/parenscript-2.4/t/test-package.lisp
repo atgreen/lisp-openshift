2a1c3563680375bbe7c47d3394c24402303bd806
Jacek Podkanski <ruby.object@googlemail.com>
2013-06-03 12:31:17 +0100

    deleted lots of files

--- quicklisp/dists/quicklisp/software/parenscript-2.4/t/test-package.lisp
+++
@@ -1,9 +0,0 @@
-(in-package #:cl)
-(named-readtables:in-readtable :parenscript)
-
-(defpackage #:ps-test
-  (:use #:cl #:parenscript #:eos)
-  (:export #:run-tests #:interface-function #:test-js-eval #:jsarray))
-
-(defpackage #:ps-eval-tests
-  (:use #:cl #:eos #:parenscript #:ps-test))
